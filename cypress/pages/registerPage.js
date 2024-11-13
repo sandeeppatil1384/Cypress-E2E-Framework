d3d98c9cff269f7b688473994ed314810a143306
sandeeppatil1384 <sandeepvpatil1984@gmail.com>
2024-11-13 11:48:16 +0530

    Registration Page using page object model

---
+++ cypress/pages/registerPage.js
@@ -0,0 +1,42 @@
+export class registerPage{
+
+    weblocators={
+        firstName:'#input-firstname',
+        lastName:'#input-lastname',
+        email:'#input-email',
+        telephone:'#input-telephone',
+        password:'#input-password',
+        passwordConfirm:'#input-confirm',
+        policyCheckbox:'input[type="checkbox"]',
+        continue:'.btn.btn-primary'
+
+    }
+
+    openURL(){
+        cy.visit(Cypress.env('URL'))
+    }
+
+    enterFirstName(FName){
+        cy.get(this.weblocators.firstName).type(FName)
+    }
+    enterLastName(LName){
+        cy.get(this.weblocators.lastName).type(LName)
+    }
+    enterEmail(email){
+        cy.get(this.weblocators.email).type(email)
+    }
+    enterTelephone(telephone){
+        cy.get(this.weblocators.telephone).type(telephone)
+    }
+    enterPassword(password){
+        cy.get(this.weblocators.password).type(password)
+        cy.get(this.weblocators.passwordConfirm).type(password)
+    }
+    selectCheckbox(){
+        cy.get(this.weblocators.policyCheckbox).check()
+    }
+    clickOnContinue(){
+        cy.get(this.weblocators.continue).click()
+    }
+    
+}
